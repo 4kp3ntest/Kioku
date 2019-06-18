9532ee4732d18ca76e74e901b3072d858c84c51d
skllrn@work <root@localhost.localdomain>
2019-06-18 10:40:40 -0400

    Added new public key

--- SETUP.sh
+++ SETUP.sh
@@ -2,16 +2,16 @@
 # [CONFIG]
 cp Configs/zshrc ~/.zshrc
 cp Configs/vimrc ~/.vimrc
-cp Configs/ ~/.vimrc
 
 # [VIM] Install vundle & YouCompleteMe
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+#:PluginInstall
+vim +PluginInstall +qall
 # Compile YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
 python3 install.py
-vim +PluginInstall +qall
-#:PluginInstall
 
 # [ZSH] Change SHELL
 chsh -s /usr/bin/zsh
